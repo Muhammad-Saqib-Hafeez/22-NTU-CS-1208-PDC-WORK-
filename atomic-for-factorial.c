5be5fd6b92da301b68ed96a4eca78aa2daa2e2df
Muhammad-Saqib-Hafeez <saqib.developer26@gmail.com>
2025-03-08 22:09:50 +0500

    Implementing atomic operation for parallel factorial

---
+++ atomic-for-factorial.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <omp.h>
+
+unsigned long long factorial(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int start = 1, end = 20; // Range of numbers
+    unsigned long long results[end - start + 1]; // Array to store results
+
+    double start_time = omp_get_wtime();
+
+    // Parallel region with atomic operation
+    #pragma omp parallel for
+    for (int i = start; i <= end; i++) {
+        int thread_id = omp_get_thread_num(); // Get thread ID
+        unsigned long long fact = factorial(i);
+        #pragma omp atomic write
+        results[i - start] = fact;
+        printf("Thread %d calculated %d! = %llu\n", thread_id, i, results[i - start]);
+    }
+
+    double end_time = omp_get_wtime();
+
+    // Print results
+    printf("Factorials from %d to %d (Atomic Operation):\n", start, end);
+    for (int i = start; i <= end; i++) {
+        printf("%d! = %llu\n", i, results[i - start]);
+    }
+
+    // Print execution time
+    printf("Execution Time (Atomic Operation): %f seconds\n", end_time - start_time);
+
+    return 0;
+}
+
+/*
+Remarks:
+- The atomic operation ensures that a specific memory location is updated atomically (without race conditions).
+- It is less expensive than a critical section but can only be used for simple operations (e.g., read, write, update).
+- Suitable for protecting single shared variables.
+*/
