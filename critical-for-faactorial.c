79f3ea102002508bfb46339c609a195c5cedb85a
Muhammad-Saqib-Hafeez <saqib.developer26@gmail.com>
2025-03-09 10:51:30 +0500

    observe the time of critical while expand range to 10000

--- critical-for-faactorial.c
+++ critical-for-faactorial.c
@@ -10,7 +10,7 @@ unsigned long long factorial(int n) {
 }
 
 int main() {
-    int start = 1, end = 20; 
+    int start = 1, end = 10000; 
     unsigned long long results[end - start + 1]; 
     double start_time = omp_get_wtime();
 
@@ -21,15 +21,15 @@ int main() {
         #pragma omp critical
         {
             results[i - start] = fact;
-            printf("Thread %d calculated %d! = %llu\n", thread_id, i, results[i - start]);
-        }
+        //     printf("Thread %d calculated %d! = %llu\n", thread_id, i, results[i - start]);
+         }
     }
     double end_time = omp_get_wtime();
 
-    printf("Factorials from %d to %d (Critical Section):\n", start, end);
-    for (int i = start; i <= end; i++) {
-        printf("%d! = %llu\n", i, results[i - start]);
-    }
+    // printf("Factorials from %d to %d (Critical Section):\n", start, end);
+    // for (int i = start; i <= end; i++) {
+    //     printf("%d! = %llu\n", i, results[i - start]);
+    // }
     printf("Execution Time (Critical Section): %f seconds\n", end_time - start_time);
     return 0;
 }
