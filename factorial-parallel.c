da45a7852090a4b4b3b079afa5bdb2eaa998a42f
Muhammad-Saqib-Hafeez <saqib.developer26@gmail.com>
2025-03-09 10:18:35 +0500

    observe the time of parallell while expand range to 10000

--- factorial-parallel.c
+++ factorial-parallel.c
@@ -11,7 +11,7 @@ unsigned long long factorial(int n) {
 }
 
 int main() {
-    int start = 1, end = 20; 
+    int start = 1, end = 10000; 
     unsigned long long results[end - start + 1]; 
 
     double start_time = omp_get_wtime();
@@ -20,15 +20,15 @@ int main() {
     for (int i = start; i <= end; i++) {
         int thread_id = omp_get_thread_num(); 
         results[i - start] = factorial(i);
-        printf("Thread %d calculated %d! = %llu\n", thread_id, i, results[i - start]);
+        // printf("Thread %d calculated %d! = %llu\n", thread_id, i, results[i - start]);
     }
 
     double end_time = omp_get_wtime();
 
-    printf("Factorials from %d to %d:\n", start, end);
-    for (int i = start; i <= end; i++) {
-        printf("%d! = %llu\n", i, results[i - start]);
-    }
+    // printf("Factorials from %d to %d:\n", start, end);
+    // for (int i = start; i <= end; i++) {
+    //     printf("%d! = %llu\n", i, results[i - start]);
+    // }
 
     printf("Execution Time (Parallel): %f seconds\n", end_time - start_time);
 
