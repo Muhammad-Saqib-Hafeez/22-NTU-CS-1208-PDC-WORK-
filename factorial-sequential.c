55142efb7cc0a1a363045796ebbf113ae96901ef
Muhammad-Saqib-Hafeez <saqib.developer26@gmail.com>
2025-03-09 10:15:09 +0500

    observe the time of sequential while expand range to 10000

--- factorial-sequential.c
+++ factorial-sequential.c
@@ -10,7 +10,7 @@ unsigned long long factorial(int n) {
 }
 
 int main() {
-    int start = 1, end = 20; 
+    int start = 1, end = 10000; 
     unsigned long long results[end - start + 1];
 
     clock_t start_time = clock();
@@ -19,10 +19,10 @@ int main() {
     }
     clock_t end_time = clock();
 
-    printf("Factorials from %d to %d:\n", start, end);
-    for (int i = start; i <= end; i++) {
-        printf("%d! = %llu\n", i, results[i - start]);
-    }
+    // printf("Factorials from %d to %d:\n", start, end);
+    // for (int i = start; i <= end; i++) {
+    //     printf("%d! = %llu\n", i, results[i - start]);
+    // }
     printf("Execution Time (Sequential): %f seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
     return 0;
